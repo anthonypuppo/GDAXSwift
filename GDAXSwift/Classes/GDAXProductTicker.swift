1d8e91017a88aeb1e9c22a2064a132b8c259458d
Janis Kirsteins <janis.kirsteins@notakey.com>
2017-12-11 12:17:20 +0200

    Removed print() statement

--- GDAXSwift/Classes/GDAXProductTicker.swift
+++ GDAXSwift/Classes/GDAXProductTicker.swift
@@ -29,8 +29,6 @@ public struct GDAXProductTicker: JSONInitializable {
 			throw GDAXError.invalidResponseData
 		}
 		
-		print(json)
-		
 		guard let tradeID = json["trade_id"] as? Int else {
 			throw GDAXError.responseParsingFailure("trade_id")
 		}
